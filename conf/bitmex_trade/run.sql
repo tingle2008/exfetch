05bf6466f80c379d9620179f3960e1eb8fac232c
Yu Ting <tingle2008@gmail.com>
2022-06-16 16:24:46 +0000

    bitmex_trade contained BTX and ETH

---
+++ conf/bitmex_trade/run.sql
@@ -0,0 +1,24 @@
+create table public.bitmex_trade ("tt" timestamp,
+                                  "symbol" varchar(6) ,
+                                  "side" varchar(5) ,
+                                  "size" integer,
+                                  "price" float,
+                                  "grossValue" float,
+                                  "homeNotional" float)
+     partition by range("tt")
+
+select 'create table jumbo.bitmex_trade_' ||
+                    extract(year from zz) ||
+   to_char(extract(month from zz),'fm00') ||
+   to_char(extract(day from zz),'fm00') ||
+$$ partition of public.bitmex_trade for values from ('$$ ||
+                                     date(zz) ||
+                                     $$')$$   ||
+                                   $$ to ('$$ ||
+           date(date(zz) + interval '1day') ||
+                                      $$');$$
+    from
+    generate_series(date_trunc('day',to_date('20220609','yyyymmdd')),
+                    date_trunc('day',to_date('20220705','yyyymmdd')),'1 day') as tt(zz);
+
+/gexec
