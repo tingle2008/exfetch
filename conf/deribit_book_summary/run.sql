b50cae17a68e34c52c4c78155b33e347773aef6e
ting <tingle2008@gmail.com>
2022-08-14 14:58:35 -0700

    .

--- conf/deribit_book_summary/run.sql
+++ conf/deribit_book_summary/run.sql
@@ -6,19 +6,17 @@ create table public.deribit_book_summary_main_option (
     "mark_price" float,
     "instrument_name" varchar(32),
     "creation_timestamp" timestamp,
-    "base_currency" varchar(4)) partition by range("creation_timestamp")
-
+    "base_currency" varchar(4)) partition by range("creation_timestamp");
 
 select 'create table jumbo.deribit_book_summary_main_option_' ||
                     extract(year from zz) ||
    to_char(extract(month from zz),'fm00') ||
-$$ partition of jumbo.deribit_book_summary_main_option for values from ('$$ ||
+$$ partition of public.deribit_book_summary_main_option for values from ('$$ ||
                                      date(zz) ||
                                      $$')$$   ||
                                    $$ to ('$$ ||
            date(date(zz) + interval '1month') ||
                                       $$');$$
     from
-            generate_series(date_trunc('month',to_date('20220601','yyyymmdd')),
-            date_trunc('month',to_date('20230101','yyyymmdd')),'1 month') as tt(zz);
-
+            generate_series(date_trunc('month',to_date('20220801','yyyymmdd')),
+            date_trunc('month',to_date('20250101','yyyymmdd')),'1 month') as tt(zz);
